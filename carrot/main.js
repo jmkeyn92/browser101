2e46212a20dab12bfc5878395972445e2ebe4e90
jmkeyn92 <82368439+jmkeyn92@users.noreply.github.com>
2022-07-09 15:45:00 +0900

    update

---
+++ carrot/main.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const timer = document.querySelector('.timer');
+const counter = document.querySelector('.counter');
+const field = document.querySelector('.field');
+const carrot = document.querySelector('.carrot-icon');
+const bug = document.querySelector('.bug-icon');
+
+let second = 10;
+
+let stopWatch = setInterval(() => {
+  second--;
+  if(second == -1) {
+    clearInterval(stopWatch);
+  } else {
+    styleChange();
+  }  
+
+
+}, 1000);
+
+function styleChange() {
+  timer.innerHTML = second;
+};
+
+
+const fieldRect = field.getBoundingClientRect();
+// console.log(fieldRect.left, fieldRect.top);
+
+function randScatter() {
+  let n = 10;
+  for(let i=0; i<n; i++) {
+    let left = (fieldRect.right-fieldRect.left)*Math.random();
+    let top = (fieldRect.bottom-fieldRect.top)*Math.random();
+    // console.log(left, top);
+    createItem(left, top, carrot);
+  }
+  for(let i=0; i<n; i++) {
+    let left = (fieldRect.right-fieldRect.left)*Math.random();
+    let top = (fieldRect.bottom-fieldRect.top)*Math.random();
+    // console.log(left, top);
+    createItem(left, top, bug);
+  }
+};
+
+function createItem(left, top, item) {
+  // console.log(left, top);
+  const node = item.cloneNode(true);
+  // console.log(node);
+  node.removeAttribute('hidden');
+  node.style.left = `${left}px`;
+  node.style.top = `${top}px`;
+  field.appendChild(node);
+};
+
+
+randScatter();
+
+const carrots = document.querySelectorAll('.carrot-icon')
+console.log(carrots);
+carrots.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    console.log(counter);
+    event.target.remove();
+  })
+});
